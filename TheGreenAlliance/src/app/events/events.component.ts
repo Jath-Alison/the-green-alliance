1b839fc48b84aca58f96af5d90fb6cafd92a21fc
Jath-Alison <Jath.Alison@gmail.com>
2024-10-17 20:35:50 -0500

    Added Loading Animation, going to continue using bootstrap

--- TheGreenAlliance/src/app/events/events.component.ts
+++ TheGreenAlliance/src/app/events/events.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
-import { EventBlockComponent } from '../event-block/event-block.component';
-import { RobotEventsAPI } from '../RobotEventsAPI';
-import { Events } from '../Schemas';
 import { Observable } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
+
+import { RobotEventsAPI } from '../RobotEventsAPI';
+import { Events } from '../Schemas';
+import { EventBlockComponent } from '../event-block/event-block.component';
+import { LoadingComponent } from '../loading/loading.component';
 
 @Component({
   selector: 'app-events',
   standalone: true,
-  imports: [EventBlockComponent, AsyncPipe],
+  imports: [EventBlockComponent, LoadingComponent, AsyncPipe],
   templateUrl: './events.component.html',
   styleUrl: './events.component.css'
 })
